25c9e10246cec82201dff0ccfd69458e36474887
fadiabji <fadi.abji@htomail.com>
2022-10-06 19:25:37 +0200

    Add project files.

---
+++ JQuery Assignment/Scripts/JQ.js
@@ -0,0 +1,8 @@
+﻿//$("body").css("background-color", "lightblue");
+
+$("body").addClass("bg-primary bodyFont");
+
+$("#textInput").keyup(function () {
+    console.log("Key Up");
+    $("#appendDiv").append("<p>" + $("#textInput").val() + "</p>")
+});
